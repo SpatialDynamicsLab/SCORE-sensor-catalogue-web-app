bcb1148285aa8bf1d23782f722e09e9c257da500
Gabriel Oduori <gabriel.oduori@gmail.com>
2023-02-06 21:55:01 +0300

    Installed Django allauth package to handle all user authentication

--- sensor_catalogue/sensor_catalogue/settings.py
+++ sensor_catalogue/sensor_catalogue/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = "django-insecure-3ltf#om(lz0ocbr8ovgm)9jhn53@c=49ucf+s*7qoq4a0+2&p(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
     "catalogue",
+
 ]
 
 MIDDLEWARE = [
@@ -135,3 +142,21 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# AUTHENTICATION 
+
+AUTHENTICATION_BACKENDS = [
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+
+
+
+SITE_ID = 1
+
