08ce3cd0e67cbda66003f194f9ee5eaf74a0d8c0
José P. Gómez B. S <jpablo.gomezb@hotmail.com>
2023-02-19 21:57:59 +0100

    Updated requirements

--- sensor_catalogue/sensor_catalogue/settings.py
+++ sensor_catalogue/sensor_catalogue/settings.py
@@ -74,25 +74,24 @@ WSGI_APPLICATION = "sensor_catalogue.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catalogue',
-        'USER': 'database_user',
-        'PASSWORD': 'P@$$word_not_defined',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
+     }
+ }
 
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'catalogue',
+#        'USER': 'database_user',
+#        'PASSWORD': 'P@$$word_not_defined',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
 
 # Password validation
