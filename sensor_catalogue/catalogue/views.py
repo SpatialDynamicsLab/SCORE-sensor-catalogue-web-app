bc626f6f1859e9305f4e98da265ed432427cefb0
José P. Gómez B. S <jpablo.gomezb@hotmail.com>
2023-02-22 17:32:59 +0100

    Updating web app message

--- sensor_catalogue/catalogue/views.py
+++ sensor_catalogue/catalogue/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def homeview(request):
+    return HttpResponse(
+      "<br>"
+      "<h1>SCORE Sensor Catalogue</h1>"
+      "<h2>We are updating the web app at this moment...</h2> ")
