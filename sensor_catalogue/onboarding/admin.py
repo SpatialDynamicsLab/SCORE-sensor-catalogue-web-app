c3e49d45ab4953c870be5e6d55b5b81f0c718192
José P. Gómez B. S. <jpablo.gomezb@hotmail.com>
2024-04-16 20:59:24 +0200

    Added filter Sensor by Name in the onboarding admin

--- sensor_catalogue/onboarding/admin.py
+++ sensor_catalogue/onboarding/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from onboarding.models import Sensor, SensorThing, InstallationStep
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import gettext_lazy as _
+from onboarding.models import SensorThing, InstallationStep
+from catalogue.models import Sensor
 
 
-# @admin.register(Sensor)
-# class OrderItemAdmin(admin.ModelAdmin):
-#     list_display = [
-#         f.name for f in Sensor._meta.fields
-#     ]
+class SensorNameFilter(SimpleListFilter):
+    title = _('Sensor by Name')
+    parameter_name = 'sensor_name'
+
+    def lookups(self, request, model_admin):
+        sensor_list = Sensor.objects.all()
+        return [(sensor.id, sensor.sensor_name) for sensor in sensor_list]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(sensor__id=self.value())
+        return queryset
 
 
 @admin.register(SensorThing)
@@ -14,6 +24,7 @@ class OrderItemAdmin(admin.ModelAdmin):
     list_display = [
         f.name for f in SensorThing._meta.fields
     ]
+    list_filter = (SensorNameFilter,)
 
 
 @admin.register(InstallationStep)
@@ -21,4 +32,5 @@ class OrderItemAdmin(admin.ModelAdmin):
     list_display = [
         f.name for f in InstallationStep._meta.fields
     ]
+    list_filter = (SensorNameFilter,)
 
